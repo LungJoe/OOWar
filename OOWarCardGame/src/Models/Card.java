64af75fb61a855b9041e568a3b0d2fef25354ffd
LungJoe <lungjoe107@gmail.com>
2017-09-28 17:36:19 -0500

    Models

---
+++ OOWarCardGame/src/Models/Card.java
@@ -0,0 +1,6 @@
+package Models;
+
+public class Card {
+    String suit;
+    int number;
+}
---
+++ OOWarCardGame/src/Models/Player.java
@@ -0,0 +1,9 @@
+package Models;
+
+import java.util.ArrayList;
+
+public class Player {
+    String name;
+    ArrayList<Card> deck;
+    
+}
