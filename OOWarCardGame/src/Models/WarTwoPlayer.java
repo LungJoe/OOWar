6c5f9e2c654daf96a1bf60abb32469892e9375c8
LungJoe <lungjoe107@gmail.com>
2017-10-06 13:30:15 -0500

    cleaning up war logic

--- OOWarCardGame/src/Models/WarTwoPlayer.java
+++ OOWarCardGame/src/Models/WarTwoPlayer.java
@@ -1,69 +1,68 @@
 package Models;
 
 public class WarTwoPlayer extends War {
-	Pile discardPile;
-	Deck deck;
-	Player playerOne;
-	Player playerTwo;
-	Card playerOneCard;
-	Card playerTwoCard;
+    Pile discardPile;
+    Deck deck;
+    Player playerOne;
+    Player playerTwo;
+    Card playerOneCard;
+    Card playerTwoCard;
+    Printer printer;
 
-	public WarTwoPlayer(Player playerOne, Player playerTwo, Deck deck) {
-		this.playerOne = playerOne;
-		this.playerTwo = playerTwo;
-		discardPile = new Pile();
-		this.deck = deck;
-	}
+    public WarTwoPlayer(Player playerOne, Player playerTwo, Deck deck) {
+	this.playerOne = playerOne;
+	this.playerTwo = playerTwo;
+	discardPile = new Pile();
+	this.deck = deck;
+	//this.printer = new Printer();
+    }
 
-	public void playGame() {
-		// boolean stillPlaying = true;
-		int testIterations = 0;
-		while (playerOne.stillHasCards() && playerTwo.stillHasCards()) {
-			playerOneCard = playerOne.getNextCard();
-			playerTwoCard = playerTwo.getNextCard();
-			if (CompareCards.CompareTwoCards(playerOneCard, playerTwoCard) == 1) {
-				System.out.printf("%s wins with %s over %s ", playerOne.getName(), playerOneCard, playerTwoCard);
-				playerOne.addCardsToDeck(playerOneCard, playerTwoCard);
-				System.out.printf("One - %d, Two - %d\n", playerOne.getDeck().getDeck().size(), playerTwo.getDeck().getDeck().size());
-			} else if (CompareCards.CompareTwoCards(playerOneCard, playerTwoCard) == 2) {
-				System.out.printf("%s wins with %s over %s ", playerTwo.getName(), playerTwoCard, playerOneCard);
-				playerTwo.addCardsToDeck(playerOneCard, playerTwoCard);
-				System.out.printf("One - %d, Two - %d\n", playerOne.getDeck().getDeck().size(), playerTwo.getDeck().getDeck().size());
-			} else {
-				war();
-			}
-			// stillPlaying = false;
-			testIterations += 1;
-		}
+    public void playGame() {
+	while (playerOne.stillHasCards() && playerTwo.stillHasCards()) {
+	    setUpRound();
+	    determineRoundWinner().addCardsToDeck(discardPile.getPile());
+	    discardPile.clearPile();
 	}
+    }
 
-	public void setPlayerDecks() {
-		playerOne.setDeck(deck.split());
-		playerTwo.setDeck(deck);
-	}
+    public void setPlayerDecks() {
+	playerOne.setDeck(deck.split());
+	playerTwo.setDeck(deck);
+    }
 
-	public void war() {
-		
-		System.out.println("WAR!");
-		discardPile.addOntoPile(playerOneCard);
-		discardPile.addOntoPile(playerTwoCard);
-		discardPile.addOntoPile(playerOne.getNextCard());
-		discardPile.addOntoPile(playerTwo.getNextCard());
-		playerOneCard = playerOne.getNextCard();
-		playerTwoCard = playerTwo.getNextCard();
-		//repeat code
-		if (CompareCards.CompareTwoCards(playerOneCard, playerTwoCard) == 1) {
-			System.out.printf("%s wins with %s over %s\n", playerOne.getName(), playerOneCard, playerTwoCard);
-			playerOne.addCardsToDeck(discardPile.getPile());
-			playerOne.addCardsToDeck(playerOneCard, playerTwoCard);
-		} else if (CompareCards.CompareTwoCards(playerOneCard, playerTwoCard) == 2) {
-			System.out.printf("%s wins with %s over %s\n", playerTwo.getName(), playerTwoCard, playerOneCard);
-			playerTwo.addCardsToDeck(discardPile.getPile());
-			playerTwo.addCardsToDeck(playerOneCard, playerTwoCard);
-		} else {
-			war();
-		}
-		discardPile.clearPile();
+    public void war() {
+
+	System.out.println("WAR!");
+	setUpWar();
+	determineRoundWinner().addCardsToDeck(discardPile.getPile());
+	discardPile.clearPile();
+    }
+
+    public void setUpRound() {
+	playerOneCard = playerOne.getNextCard();
+	playerTwoCard = playerTwo.getNextCard();
+	discardPile.addOntoPile(playerOneCard);
+	discardPile.addOntoPile(playerTwoCard);
+	
+    }
+    
+    public void setUpWar() {
+	discardPile.addOntoPile(playerOne.getNextCard());
+	discardPile.addOntoPile(playerTwo.getNextCard());
+	setUpRound();
+    }
+
+    public Player determineRoundWinner() {
+	if (CompareCards.CompareTwoCards(playerOneCard, playerTwoCard) == 1) {
+	    System.out.printf("%s wins with %s over %s\n", playerOne.getName(), playerOneCard, playerTwoCard);
+	    playerOne.addCardsToDeck(playerOneCard, playerTwoCard);
+	} else if (CompareCards.CompareTwoCards(playerOneCard, playerTwoCard) == 2) {
+	    System.out.printf("%s wins with %s over %s\n", playerTwo.getName(), playerTwoCard, playerOneCard);
+	    playerTwo.addCardsToDeck(playerOneCard, playerTwoCard);
+	} else {
+	    war();
 	}
+	return playerOne;
+    }
 
 }
